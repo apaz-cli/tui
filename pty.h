5c6f83006f72623ee3613d5aa96ddcb101ef0825
apaz-cli <aarpazdera@gmail.com>
2022-04-29 15:03:32 -0500

    Added pty open implementation.

---
+++ pty.h
@@ -0,0 +1,23 @@
+#include <fcntl.h>
+#include <stdlib.h>
+
+static inline int tui_pty_open(int* master_fd, int* slave_fd) {
+    int masterfd, slavefd;
+    char *slavedevice;
+
+    masterfd = posix_openpt(O_RDWR | O_NOCTTY);
+
+    if ( masterfd == -1 ||
+         grantpt (masterfd) == -1 ||
+         unlockpt (masterfd) == -1 ||
+         (slavedevice = ptsname (masterfd)) == NULL)
+        return 1;
+
+    slavefd = open(slavedevice, O_RDWR|O_NOCTTY);
+    if (slavefd < 0)
+        return 1;
+
+    *master_fd = masterfd;
+    *slave_fd = slavefd;
+    return 0;
+}
